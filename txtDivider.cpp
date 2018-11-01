ca430ace55ccdfb65633d2e96b63ed775510f70a
DandelionLau <dandelionlau@163.com>
2018-11-01 11:15:17 +0800

    Update and rename divideTxt.cpp to txtDivider.cpp

--- txtDivider.cpp
+++ txtDivider.cpp
@@ -1,6 +1,5 @@
-// halfTxt.cpp : 定义控制台应用程序的入口点。
-/*	divide txt into two parts and save in different fold
-	copyright©2017DandelionLau 
+/*	
+divide txt into two parts and save in different folder
 */
 
 #include "stdafx.h"
@@ -28,17 +27,17 @@ int main(){
 		for(i=0;i<5;i++){
 
 				sprintf(inFilePath,"F:\\test_%d\\%s.txt",rate*10,eFileName[i]);
-				printf("%s\n",inFilePath);//get input file  path
+				printf("%s\n",inFilePath);		//get input file  path
 				sprintf(trainFilePath,"F:\\test_%d\\%s\\train.txt",rate*10,cFileName[i]);
-				printf("%s\n",trainFilePath);//get train.txt path
+				printf("%s\n",trainFilePath);		//get train.txt path
 				sprintf(testFilePath,"F:\\test_%d\\%s\\test.txt",rate*10,cFileName[i]);
-				printf("%s\n",testFilePath);//get test.txt path
+				printf("%s\n",testFilePath);		//get test.txt path
 
-				fstream data;//source file 
-				ofstream train;//target file1
-				ofstream test;//target file2
+				fstream data;				//source file 
+				ofstream train;				//target file1
+				ofstream test;				//target file2
 				string buff;
-				int mid = 0,count = 0,line = 0;//mid the:txt's lines, count :txt's total lines,line :counter
+				int mid = 0,count = 0,line = 0;		//mid the:txt's lines, count :txt's total lines,line :counter
 
 				data.open(inFilePath,ios::in);
 				if (!data.is_open())
@@ -46,10 +45,10 @@ int main(){
 					  cout << "Can not find target file." << endl;
 					  return 0;
 				 }
-				while(getline(data, buff)){count++;}//get total lines
-				//cout<<"count ="<<count<<endl;
+				while(getline(data, buff)){count++;}	//get total lines
+									//cout<<"count ="<<count<<endl;
 				mid = count/2;
-				//cout<<"mid ="<<mid<<endl;
+									//cout<<"mid ="<<mid<<endl;
 				data.close();
 				
 				data.open(inFilePath,ios::in);
@@ -63,7 +62,7 @@ int main(){
 				while(getline(data, buff)) { 
 					
 					if(line>mid){break;}
-					train<<buff<<endl;//write first-half into train.txt
+					train<<buff<<endl;		//write first-half into train.txt
 					line++;
 				 }
 				data.close();
@@ -80,7 +79,7 @@ int main(){
 				test.open(testFilePath,ios::app);
 			    while(getline(data, buff)) { 
 					if(line>mid){	
-					test<<buff<<endl;//write last-half into test.tx		
+					test<<buff<<endl;		//write last-half into test.tx		
 					}//end if
 					line++;
 				
